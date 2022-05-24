8b3e53aef31138cc4b3bb5b2ab73d87d7bd058f3
Jessica <jessicambromley7@gmail.com>
2022-05-24 19:25:04 -0400

    Update sumAll to return error on non-number arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1, num2) {
     let smallerNum = null;
     let biggerNum = null;
 
-    if(num1 < 0 || num2 < 0){
+    if(num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number'){
         return 'ERROR'; 
     }
 
