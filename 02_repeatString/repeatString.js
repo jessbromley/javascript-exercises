a0c367d6d58c769d9ee46a76b627da8baa1a0c9d
Jessica <jessicambromley7@gmail.com>
2022-05-24 17:40:40 -0400

    Add parameters, loop, and return

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    let fullString = '';
+    for (let i = 0; i < num; i++) {
+        fullString += str;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
