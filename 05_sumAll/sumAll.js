115ff0495df0e29320f5e93fa87705ff6faee011
Jessica <jessicambromley7@gmail.com>
2022-05-24 19:12:46 -0400

    Add inital solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let smallerNum = null;
+    let biggerNum = null;
+    if(num1 > num2){
+        biggerNum = num1;
+        smallerNum = num2;
+    } else {
+        biggerNum = num2;
+        smallerNum = num1;
+    }
 
+    let total = 0;
+
+    for(let i = smallerNum; i <= biggerNum; i++){
+        total += i;
+    }
+    return total;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
