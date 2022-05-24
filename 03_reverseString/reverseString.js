79ef4c70ed0faab559a84bbfc237521e2dc6828a
Jessica <jessicambromley7@gmail.com>
2022-05-24 18:30:12 -0400

    Add inital solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let revString = '';
 
+    for (let i = -1; i > -(str.length+1); i--) {
+        revString += str.substr(i, 1);
+    }
+    return revString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
